c19da3f539f8721457bc9e329580757bce4a7f38
madhavtrivedi77 <75065978+madhavtrivedi77@users.noreply.github.com>
2023-01-05 02:38:07 +0530

    i18n using transloco demo (en -> fr)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslocoService } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'native-i18n';
+  title = "native-i18n";
+  siteLanguage = 'English';
+  
+  languageList = [
+    { code: 'en', label: 'English' },
+    { code: 'fr', label: 'French' }
+  ];
+  
+  constructor(private service: TranslocoService) { 
+    this.service.selectTranslate('app-name').subscribe(value => {this.title = value});
+  }
+  
+  changeSiteLanguage(language: string): void {
+    this.service.setActiveLang(language);
+    this.service.selectTranslate('app-name').subscribe(value => {this.title = value});
+    this.siteLanguage = this.languageList.find(f => f.code === language)?.label || 'English';
+  }
 }
